2ac9e12085b46303dd39336347f9f309a044ba4c
Devendra-1997 <devendradhakal92@gmail.com>
2024-07-15 04:35:19 +1000

    add new ticket form is added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
-// import { Entry } from "./page/entry/Entry.page";
+import { Entry } from "./pages/entry/Entry.page";
 import { DefaultLayout } from "./layout/DefaultLayout";
 import { Dashboard } from "./pages/dashboard/Dashboard.page";
+import { AddTicket } from "./pages/new-ticket/AddTicket.page";
 
 function App() {
   return (
     <div className="App">
-      {/* <Entry /> */}
+      <Entry />
       <DefaultLayout>
         <Dashboard />
+        <AddTicket />
       </DefaultLayout>
     </div>
   );
