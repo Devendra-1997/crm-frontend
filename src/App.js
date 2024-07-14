2402d2c247782d4ae8dcaf2e1ba1f48585213e60
Devendra-1997 <devendradhakal92@gmail.com>
2024-07-15 02:32:13 +1000

    added dashboard page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 // import { Entry } from "./page/entry/Entry.page";
 import { DefaultLayout } from "./layout/DefaultLayout";
+import { Dashboard } from "./pages/dashboard/Dashboard.page";
 
 function App() {
   return (
     <div className="App">
       {/* <Entry /> */}
-      <DefaultLayout>Dashboard</DefaultLayout>
+      <DefaultLayout>
+        <Dashboard />
+      </DefaultLayout>
     </div>
   );
 }
