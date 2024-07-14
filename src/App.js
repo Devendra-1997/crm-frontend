f18702abfec5820102974ebde5a2600ef8750bd5
Devendra-1997 <devendradhakal92@gmail.com>
2024-07-15 02:18:29 +1000

    layout is added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
-import { Entry } from "./page/entry/Entry.page";
+// import { Entry } from "./page/entry/Entry.page";
+import { DefaultLayout } from "./layout/DefaultLayout";
 
 function App() {
   return (
     <div className="App">
-      <Entry />
+      {/* <Entry /> */}
+      <DefaultLayout>Dashboard</DefaultLayout>
     </div>
   );
 }
