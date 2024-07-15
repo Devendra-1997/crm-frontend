56c09a8751e6456b716afb2348fcf16f3120e38a
Devendra-1997 <devendradhakal92@gmail.com>
2024-07-15 19:54:04 +1000

    addede ticket lists pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Entry } from "./pages/entry/Entry.page";
 import { DefaultLayout } from "./layout/DefaultLayout";
 import { Dashboard } from "./pages/dashboard/Dashboard.page";
 import { AddTicket } from "./pages/new-ticket/AddTicket.page";
+import { TicketLists } from "./pages/ticket-list/TicketLists.page";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <DefaultLayout>
         <Dashboard />
         <AddTicket />
+        <TicketLists />
       </DefaultLayout>
     </div>
   );
